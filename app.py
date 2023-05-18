bf3811b64b967f42e453f18a0e705c17fbcdf7ee
XaviAlbert <130755203+XaviAlbert@users.noreply.github.com>
2023-05-18 18:02:47 +0200

    d2 cod2

--- app.py
+++ app.py
@@ -18,3 +18,8 @@ train_df['Sex'] = train_df['Sex'].map({'male': 0, 'female': 1})
 test_df['Sex'] = test_df['Sex'].map({'male': 0, 'female': 1})
 train_df['Sex'].fillna(train_df['Age'].mean(), inplace=True)
 test_df['Sex'].fillna(test_df['Age'].mean(), inplace=True)
+
+train_df['Age'].fillna(train_df['Age'].mean(), inplace=True)
+test_df['Age'].fillna(test_df['Age'].mean(), inplace=True)
+train_df['Embarked'].fillna(train_df['Embarked'].mode()[0], inplace=True)
+test_df['Embarked'].fillna(test_df['Embarked'].mode()[0], inplace=True)
