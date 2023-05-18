23ff16884e8f8f4b1115e9de8459d3ce592e688b
XaviAlbert <130755203+XaviAlbert@users.noreply.github.com>
2023-05-18 18:20:15 +0200

    d2 p1

--- app.py
+++ app.py
@@ -23,3 +23,4 @@ train_df['Age'].fillna(train_df['Age'].mean(), inplace=True)
 test_df['Age'].fillna(test_df['Age'].mean(), inplace=True)
 train_df['Embarked'].fillna(train_df['Embarked'].mode()[0], inplace=True)
 test_df['Embarked'].fillna(test_df['Embarked'].mode()[0], inplace=True)
+
