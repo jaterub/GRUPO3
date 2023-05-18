901cd656848040aa0de4bb3eee76cb941c11dc31
unknown <braulio2297@gmail.com>
2023-05-18 18:56:29 +0200

    developer 3 part 2

--- app.py
+++ app.py
@@ -46,3 +46,13 @@ variables=['Pclass', 'Age', 'Sex', 'SibSp', 'Parch', 'Fare', 'CabinBool', 'Embar
 X = train_df[variables]
 y = train_df['Survived']
 
+'''
+Creamos el modelo
+'''
+X = train_df[['Pclass', 'Age', 'Sex', 'SibSp', 'Parch', 'Fare', 'CabinBool', 'Embarked_C', 
+'Embarked_S', 'Embarked_Q']]
+y = train_df[['Survived']]
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+model.fit(X_train, y_train)
+
