156a87eb4f52bef29e0da2ba7c5d909765e8f9dd
Javier Tejero <ja.tejeror@gmail.com>
2023-05-18 16:58:57 +0200

    add codigo 2

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+# Codigo 2
+
+import pandas
+import numpy
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+st.set_option('deprecation.showPyplotGlobalUse', False)
