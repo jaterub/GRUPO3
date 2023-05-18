8374ed294fc16c2fe149e5cd8252306b422c70d7
Javier Tejero <ja.tejeror@gmail.com>
2023-05-18 17:05:54 +0200

    add codigo 3

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 st.set_option('deprecation.showPyplotGlobalUse', False)
+
+train_df = pd.read_csv('data/train.csv')
+test_df = pd.read_csv('data/test.csv')
